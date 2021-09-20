6a640b9959be020617259b1b74d98e1cc22c7cf4
Alexpol <alexpol2905@gmail.com>
2021-09-20 22:21:09 +0300

    make show page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,7 +169,7 @@
             <div class="row">
                 <div class="col">
                     <div class="avds_xl_container clearfix">
-                        <div class="avds_xl_background" style="background-image:url(images/avds_xl.jpg)"></div>
+                        <div class="avds_xl_background" style="background-image:url(/images/avds_xl.jpg)"></div>
                         <div class="avds_xl_content">
                             <div class="avds_title">Amazing Devices</div>
                             <div class="avds_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus.</div>
