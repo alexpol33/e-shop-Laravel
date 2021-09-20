02059752ca545596b617049b058be30b10ab1a9f
Alexpol <alexpol2905@gmail.com>
2021-09-20 22:24:49 +0300

    add availability

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -60,7 +60,11 @@
                         <!-- In Stock -->
                         <div class="in_stock_container">
                             <div class="availability">Availability:</div>
-                            <span>In Stock</span>
+                            @if($item->in_stock)
+                                <span>In Stock</span>
+                            @else
+                                <span style="color: red">Not available</span>
+                            @endif
                         </div>
                         <div class="details_text">
                             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris consequat nisi ut mauris efficitur lacinia.</p>
