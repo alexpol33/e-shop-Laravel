f03abd6ba5222739a4e7463a0ff18137d5959183
Alexpol <alexpol2905@gmail.com>
2021-09-25 13:09:02 +0300

    make show images

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -36,15 +36,31 @@
                 <!-- Product Image -->
                 <div class="col-lg-6">
                     <div class="details_image">
-                        <div class="details_image_large"><img src="/images/details_1.jpg" alt=""><div class="product_extra product_new"><a href="categories.html">New</a></div></div>
+                        @php
+                            $image = '';
+                            if(count($item->images) > 0){
+                                $image = $item->images[0]['name'];
+                            } else {
+                                $image = 'no_image.png';
+                            }
+                        @endphp
+                        <div class="details_image_large"><img src="/images/{{$image}}" alt="{{$item->title}}"><div class="product_extra product_new"><a href="categories.html">New</a></div></div>
                         <div class="details_image_thumbnails d-flex flex-row align-items-start justify-content-between">
-                            <div class="details_image_thumbnail active" data-image="images/details_1.jpg"><img src="/images/details_1.jpg" alt=""></div>
-                            <div class="details_image_thumbnail" data-image="images/details_2.jpg"><img src="/images/details_2.jpg" alt=""></div>
-                            <div class="details_image_thumbnail" data-image="images/details_3.jpg"><img src="/images/details_3.jpg" alt=""></div>
-                            <div class="details_image_thumbnail" data-image="images/details_4.jpg"><img src="/images/details_4.jpg" alt=""></div>
+                            @if($image == 'no_image.png')
+
+                            @else
+                                @foreach($item->images as $img)
+                                    @if($loop->first)
+                                        <div class="details_image_thumbnail active" data-image="/images/{{$img['name']}}"><img src="/images/{{$img['name']}}" alt="{{$item->title}}"></div>
+                                    @else
+                                        <div class="details_image_thumbnail" data-image="/images/{{$img['name']}}"><img src="/images/{{$img['name']}}" alt="{{$item->title}}"></div>
+                                    @endif
+                                @endforeach
+                            @endif
                         </div>
                     </div>
                 </div>
+
 
                 <!-- Product Content -->
                 <div class="col-lg-6">
@@ -67,7 +83,7 @@
                             @endif
                         </div>
                         <div class="details_text">
-                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris consequat nisi ut mauris efficitur lacinia.</p>
+                            <p></p>
                         </div>
 
                         <!-- Product Quantity -->
