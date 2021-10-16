ab06d61fdd4997900c040544d70fa940458c1a0e
Alexpol <alexpol2905@gmail.com>
2021-10-16 13:40:38 +0300

    add users counter to admin panel

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
    public function index(){
        $products_count = Product::all()->count();
+       $users_count = User::all()->count();
 
         return view('admin.index', [
-            'products_count' => $products_count
+            'products_count' => $products_count,
+            'users_count' => $users_count
         ]);
    }
 }
